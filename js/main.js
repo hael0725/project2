8707ac59a748bed70ebd6a070aa6509f0a14189d
hael0725 <hael0725@naver.com>
2025-04-18 17:58:44 +0900

    20250418

---
+++ js/main.js
@@ -0,0 +1,30 @@
+// 채크버튼
+$(function () {
+    $('#Check button:nth-of-type(1)').click(function () {
+      $('.color').fadeIn();
+      $('.si').hide();
+      $('.vin').hide();
+
+      $('#b1').css('text-decoration','underline');
+      $('#b2').css('text-decoration','none');
+      $('#b3').css('text-decoration','none');
+    });
+    $('#Check button:nth-of-type(2)').click(function () {
+      $('.si').fadeIn();
+      $('.color').hide();
+      $('.vin').hide();
+
+      $('#b2').css('text-decoration','underline');
+      $('#b1').css('text-decoration','none');
+      $('#b3').css('text-decoration','none');
+    });
+    $('#Check button:nth-of-type(3)').click(function () {
+      $('.vin').fadeIn();
+      $('.color').hide();
+      $('.si').hide();
+
+      $('#b3').css('text-decoration','underline');
+      $('#b1').css('text-decoration','none');
+      $('#b2').css('text-decoration','none');
+    });
+  });
